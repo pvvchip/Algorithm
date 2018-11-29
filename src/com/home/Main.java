11e7d8c89aab93d5c02be439a6be7cd644c04797
pvv <pvvchip@mail.ru>
2018-11-29 06:41:07 +0300

    lesson 2 - Сортировки: Выбора, Вставки

--- src/com/home/Main.java
+++ src/com/home/Main.java
@@ -3,23 +3,35 @@ package com.home;
 import java.util.Random;
 
 public class Main {
+    private static int NUMBER_ITEM = 1000000;
+    private static  int NUMBER_MAX = 1000000;
+    private static MyArrayList<Integer> myArrayList = new MyArrayList<>();
 
     public static void main(String[] args) {
-        int NUMBER_ITEM = 1000000;
-        int NUMBER_MAX = 1000000;
-        MyArrayList<Integer> myArrayList = new MyArrayList<>();
-
-        Random rnd = new Random();
         long start, finish;
 
         myArrayList.test(myArrayList);
-        myArrayList.clean();
 
+        System.out.println("Сортировка методом Выбора");
+        newAL();
         start = System.currentTimeMillis();
+        myArrayList.selectionSort();
+        finish = System.currentTimeMillis();
+        System.out.println(finish - start + "ms");
+
+        System.out.println("Сортировка методом Вставки");
+        newAL();
+        start = System.currentTimeMillis();
+        myArrayList.insertionSort();
+        finish = System.currentTimeMillis();
+        System.out.println(finish - start + "ms");
+    }
+
+    private static void newAL() {
+        myArrayList.clean();
+        Random rnd = new Random();
         for (int i = 0; i < NUMBER_ITEM; i++) {
             myArrayList.insert(rnd.nextInt(NUMBER_MAX));
         }
-        finish = System.currentTimeMillis();
-        System.out.println(finish - start + "ms");
     }
 }
